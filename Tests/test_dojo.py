c094a3e978f523d026d69ba75bfb13dbd2681b5f
Innocent Asiimwe <asiimwe@outlook.com>
2017-04-26 09:40:38 +0300

    adding tests for create_person method

--- Tests/test_dojo.py
+++ Tests/test_dojo.py
@@ -24,47 +24,46 @@ class TestDojo(unittest.TestCase):
         """The room created should be either office or living space"""
         new_room = self.dojo.create_room('office', 'Germany')
         self.assertTrue([new_room[0].typ, new_room[0].name] == ['office', 'Germany'])
-    #
-    #
+    
     def test_room_created_only_accepts_strings(self):
         """Input both inputs should be strings """
         self.assertRaises(TypeError, self.dojo.create_room, 5, 2)
         self.assertRaises(TypeError, self.dojo.create_room, 'office', 7)
         self.assertRaises(TypeError, self.dojo.create_room, 8, '')
 
-    # def test_create_room_only_accepts_type_office_living(self):
-    #     """The type argument should accept only office and livingspace"""
-    #     self.assertRaises(ValueError, self.dojo.create_room, 'bedroom', 'Germany')
+    def test_create_room_only_accepts_type_office_living(self):
+        """The type argument should accept only office and livingspace"""
+        self.assertRaises(ValueError, self.dojo.create_room, 'bedroom', 'Germany')
 
 
+
+    def test_add_person_success(self):
+        """A person should be added to"""
+        before_count = dojo.number_of_people
+        new_person = dojo.createPerson('patrick', 'fellow', 'y')
+        after_count = dojo.number_of_people
+        self.assertEqual(before_count + 1, after_count)
+
+    def test_person_is_not_already_added(self):
+        """Person can be only added once"""
+        pass
+
+    def test_person_added_has_intended_attributes(self):
+        new_person = dojo.createPerson('patrick', 'fellow', 'y')
+        assertEqual([new_person.name, new_person.type], ['patrick', 'fellow'])
+
+
+    # def test_person_is_fellow_or_staff(self):
+    #     """Person created must be either an instance of fellow or staff"""
+    #     new_person1 = dojo.createPerson('patrick', 'fellow', 'y')
+    #     new_person2 = dojo.createPerson('john', 'staff')
     #
-    # def test_add_person_success(self):
-    #     """A person should be added to"""
-    #     before_count = dojo.number_of_people
-    #     new_person = dojo.createPerson('patrick', 'fellow', 'y')
-    #     after_count = dojo.number_of_people
-    #     self.assertEqual(before_count + 1, after_count)
-    #
-    # def test_person_is_not_already_added(self):
-    #     """Person can be only added once"""
-    #     pass
-    #
-    # def test_person_added_has_intended_attributes(self):
-    #     new_person = dojo.createPerson('patrick', 'fellow', 'y')
-    #     assertEqual([new_person.name, new_person.type], ['patrick', 'fellow'])
-    #
-    #
-    # # def test_person_is_fellow_or_staff(self):
-    # #     """Person created must be either an instance of fellow or staff"""
-    # #     new_person1 = dojo.createPerson('patrick', 'fellow', 'y')
-    # #     new_person2 = dojo.createPerson('john', 'staff')
-    # #
-    # #     assertTrue(IsInstance(new_person1, Fellow) == IsInstance(new_person2, Staff))
-    #
-    #
-    # def test_add_person_raises_typeerror_for_nonstrings(self):
-    #     """arguments must be strings"""
-    #     new_person1 = dojo.createPerson(78, 'fellow', 'y')
+    #     assertTrue(IsInstance(new_person1, Fellow) == IsInstance(new_person2, Staff))
+
+
+    def test_add_person_raises_typeerror_for_nonstrings(self):
+        """arguments must be strings"""
+        new_person1 = dojo.createPerson(78, 'fellow', 'y')
     #
     #
     # def test_add_person_raises_valueerror_for_type_not_staff_or_fellow(self):
